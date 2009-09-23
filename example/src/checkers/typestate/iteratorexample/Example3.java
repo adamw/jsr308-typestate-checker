8b821d99a3ca84899994afeacfdefafba746a3ce
Adam Warski <adam@warski.org>
2009-09-23 17:09:54 +0200

    More stubs

--- example/src/checkers/typestate/iteratorexample/Example3.java
+++ example/src/checkers/typestate/iteratorexample/Example3.java
@@ -1,14 +1,13 @@
 package checkers.typestate.iteratorexample;
 
-import java.util.Iterator;
-import java.util.Collection;
+import java.util.*;
 
 /**
  * Reading from stream with a recovery function.
  * @author Adam Warski (adam at warski dot org)
  */
 public class Example3 {
-	public void test(Collection coll) {
+	public void test(List coll) {
 		Iterator iter = coll.iterator();
 		while (iter.hasNext()) {
 			iter.next();
