1407cd5fec93fdf8b95b160477fc703183580814
Adam Warski <adam@warski.org>
2009-09-23 18:56:54 +0200

    Nicer examples

--- example/example.sh
+++ example/example.sh
@@ -4,23 +4,22 @@
 ### Fill this in
 ###
 
-export JAVA_HOME=/Users/adamw/jsr308/soylatte16-i386-1.0.3
-export CHECKERS_HOME=/Users/adamw/jsr308/checkers
+export JSR308=/Users/adamw/jsr308/checkers
 
 ###
 
-export PATH=$JAVA_HOME/bin:$PATH
-export CLASSPATH=$CHECKERS_HOME/checkers.jar:typestate-checker.jar
+export PATH=$JSR308/binary:$PATH
+export CLASSPATH=$JSR308/checkers.jar:typestate-checker.jar
 
 ### We want to use the typestate checker.
 CHECKER=checkers.typestate.TypestateChecker
 
 ### Select which example you want to check. You can also specify your own sources here.
 
-SOURCES=src/checkers/typestate/ioexample/Example1.java
+#SOURCES=src/checkers/typestate/ioexample/Example1.java
 #SOURCES=src/checkers/typestate/ioexample/Example2.java
 #SOURCES=src/checkers/typestate/iteratorexample/Example3.java
-#SOURCES=src/checkers/typestate/iteratorexample/Example4.java
+SOURCES=src/checkers/typestate/iteratorexample/Example4.java
 
 SOURCEPATH=jdk:src:states
 
